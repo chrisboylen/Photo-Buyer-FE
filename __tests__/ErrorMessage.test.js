5df186cae9ce97ccf53f43400e6543ae1a791ae5
Chris Boylen <billpolster@gmail.com>
2019-03-12 12:07:06 -0600

    Add and pass test ErrorMessage renders and matches snapshot with error

--- __tests__/ErrorMessage.test.js
+++ __tests__/ErrorMessage.test.js
@@ -3,20 +3,47 @@ import { mount } from "enzyme";
 import toJSON from "enzyme-to-json";
 import wait from "waait";
 import { MockedProvider } from "react-apollo/test-utils";
+import { CURRENT_USER_QUERY } from "../components/User";
+import { fakeUser, fakeCartItems } from "../lib/testUtils";
+
+const mocks1 = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItems()]
+        }
+      }
+    },
+    error: new Error("Broken!")
+  }
+];
 
 describe("ERROR MESSAGE", () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = mount(
+  it("renders and matches snapshot without error", async () => {
+    let wrapper = mount(
       <MockedProvider>
         <DisplayError />
       </MockedProvider>
     );
+    const error = wrapper.find("DisplayError");
+    await wait();
+    wrapper.update();
+
+    expect(toJSON(error)).toMatchSnapshot();
   });
 
-  it("renders and matches snapshot without error", async () => {
+  it("renders and matches snapshot with error", async () => {
+    let wrapper = mount(
+      <MockedProvider mocks={mocks1}>
+        <DisplayError />
+      </MockedProvider>
+    );
     const error = wrapper.find("DisplayError");
+    await wait();
+    wrapper.update();
 
     expect(toJSON(error)).toMatchSnapshot();
   });
